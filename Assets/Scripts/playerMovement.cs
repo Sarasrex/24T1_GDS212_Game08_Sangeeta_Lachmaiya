0a89656f90b0ee6c199c88b5e4eeef1fedff4d6a
Sangeeta Lachmaiya <1031886@student.sae.edu.au>
2024-04-12 12:02:33 +1000

    in class fixes during play test

--- Assets/Scripts/playerMovement.cs
+++ Assets/Scripts/playerMovement.cs
@@ -54,11 +54,11 @@ public class PlayerMovement : MonoBehaviour
         rb.velocity = new Vector3(movement.x, rb.velocity.y, movement.z);
 
         // Mouse Look
-        mouseX = Input.GetAxisRaw("Mouse X") * mouseSensitivity * Time.deltaTime;
-        mouseY = Input.GetAxisRaw("Mouse Y") * mouseSensitivity * Time.deltaTime;
+        //mouseX = Input.GetAxisRaw("Mouse X") * mouseSensitivity * Time.deltaTime;
+        //mouseY = Input.GetAxisRaw("Mouse Y") * mouseSensitivity * Time.deltaTime;
 
-        xRotation -= mouseY;
-        xRotation = Mathf.Clamp(xRotation, -90f, 90f);
+        //xRotation -= mouseY;
+        //xRotation = Mathf.Clamp(xRotation, -90f, 90f);
 
         playerCamera.transform.localRotation = Quaternion.Euler(xRotation, 0f, 0f);
         transform.Rotate(Vector3.up * mouseX);
